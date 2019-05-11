243a40eabf2420a1820df74410acc28a2896d277
Nir Tamir <nirtamir2@gmail.com>
2019-05-11 14:30:30 +0300

    typescript config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,8 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
